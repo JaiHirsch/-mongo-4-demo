43c04746972458c98aa21c2cd2a36cc7e9d99f1b
Jai Hirsch <jai.hirsch@gmail.com>
2018-05-14 16:23:27 -0500

    added license to java clases

--- src/main/java/org/mongo/DemoMongoConnector.java
+++ src/main/java/org/mongo/DemoMongoConnector.java
@@ -1,5 +1,21 @@
 package org.mongo;
 
+/**
+ This file is part of mongo-4-demo.
+ mongo-4-demo is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ mongo-4-demo is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with mongo-4-demo.  If not, see <http://www.gnu.org/licenses/>.
+ @Author Jai Hirsch
+ @github https://github.com/JaiHirsch/mongo-4-demo
+ */
+
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
